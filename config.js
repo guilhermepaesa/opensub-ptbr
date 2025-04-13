e38cb525c122ff1d920d7b595d40d754c2fdc732
guilhermepaesa <guilhermepaesa@gmail.com>
2025-04-13 17:49:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ var config = {
 
 switch (env) {
     case 'beamup':
-		config.port = process.env.PORT
+        config.port = process.env.PORT
         config.local = "https://2ecbbd610840-opensubtitles.baby-beamup.club"
         break;
 
     case 'local':
-		config.port = 64563
-        config.local = "http://127.0.0.1:" + config.port;
+        config.port = 7700
+        config.local = "https://stremio-streams.aimixtech.com:8443";
         break;
 }
 
